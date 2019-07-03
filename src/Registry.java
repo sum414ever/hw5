8e84f70794593b9fef00e7e928d2f054dc9494d0
sum414ever <copokaalekc@gmail.com>
2019-07-03 12:54:06 +0300

    Dev: fixed reamarks by Michael Yevtushenko

--- src/Registry.java
+++ src/Registry.java
@@ -1,27 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Registry {
-    Map<String, String> accounting = new HashMap<>();
+    Map<String, List<String>> accounting = new HashMap<>();
+    List <String> books = new ArrayList<>();
+
+    public void addBookToRegister(String date, String book){
+        books.add(book);
+        accounting.put(date, books);
+    }
 
     public void takeABook(String date, String book) {
-        accounting.put(date, book);
+        addBookToRegister(date, book);
         System.out.println("Today " + date + " you took the book " + book);
     }
 
     public void whenTheBookIsTaken(String d) {
-        if (accounting.get(d) == null) {
+        if (accounting.containsKey(d)) {
             System.out.println("On this date " + d + " didn't take a book");
         } else System.out.println("On this date " + d + " took this book " + accounting.get(d));
     }
 
     public void loadRegister() {
-        accounting.put("26/01/2019", "In Search of Lost Time by Marcel Proust");
-        accounting.put("26/02/2019", "Don Quixote by Miguel de Cervantes");
-        accounting.put("26/01/2018", "Ulysses by James Joyce");
-        accounting.put("20/01/2019", "The Great Gatsby by F. Scott Fitzgerald");
-        accounting.put("26/05/2019", "Moby Dick by Herman Melville");
+        addBookToRegister("26/01/2019", "In Search of Lost Time by Marcel Proust");
+        addBookToRegister("26/02/2019", "Don Quixote by Miguel de Cervantes");
+        addBookToRegister("26/01/2018", "Ulysses by James Joyce");
+        addBookToRegister("20/01/2019", "The Great Gatsby by F. Scott Fitzgerald");
+        addBookToRegister("26/05/2019", "Moby Dick by Herman Melville");
     }
 
     public void getDateInformation() {
@@ -33,7 +37,7 @@ public class Registry {
 
     public void getBookInformation() {
         System.out.println("\nAll issued books: ");
-        for (String value : accounting.values()) {
+        for (String value : books) {
             System.out.println(value);
         }
     }
